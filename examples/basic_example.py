dc3ed821528221a9dbbdbaaa15684f7601761efc
Oli <55298769+xMistt@users.noreply.github.com>
2020-01-05 00:22:26 +0000

    Update basic_example.py

--- examples/basic_example.py
+++ examples/basic_example.py
@@ -1,1 +1,10 @@
-pass
+import ScuffedAPI
+import asyncio
+
+async def arrow():
+    banner = await ScuffedAPI.get_banners(name="Arrow")
+    print(banners.id)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(arrow())
+loop.close()
